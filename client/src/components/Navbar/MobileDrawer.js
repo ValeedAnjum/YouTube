8ed7c47ef309b06f2a350d11e25883840b955b8e
ValeedAnjum <allinone644@gmail.com>
2021-01-27 00:58:05 +0500

    working on drawer

--- client/src/components/Navbar/MobileDrawer.js
+++ client/src/components/Navbar/MobileDrawer.js
@@ -1,9 +1,24 @@
 import React, { useState } from "react";
 import Drawer from "@material-ui/core/Drawer";
-import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import {
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+} from "@material-ui/core";
 import NotificationsIcon from "@material-ui/icons/Notifications";
+import MenuIcon from "@material-ui/icons/Menu";
 
+import YouTubeIcon from "./Icon/Logo";
+const useStyles = makeStyles((theme) => {
+  return {
+    toolbar: theme.mixins.toolbar,
+  };
+});
 const MobileDrawer = ({ mobileDrawer, setMobileDrawerState }) => {
+  const classes = useStyles();
   return (
     <Drawer
       variant="temporary"
@@ -11,6 +26,25 @@ const MobileDrawer = ({ mobileDrawer, setMobileDrawerState }) => {
       open={mobileDrawer}
       onClose={setMobileDrawerState}
     >
+      <div
+        className={classes.toolbar}
+        style={{ borderBottom: "1px solid #0000001a" }}
+      >
+        <List style={{ padding: "0" }}>
+          <ListItem>
+            <ListItemIcon>
+              <IconButton>
+                <MenuIcon />
+              </IconButton>
+            </ListItemIcon>
+            <ListItemText>
+              <div style={{ width: "80px", cursor: "pointer" }}>
+                <YouTubeIcon />
+              </div>
+            </ListItemText>
+          </ListItem>
+        </List>
+      </div>
       <List style={{ width: "240px" }}>
         <ListItem button>
           <ListItemIcon>
